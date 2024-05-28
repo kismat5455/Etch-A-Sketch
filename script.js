475e7c4d03a3354b68e3294bd21bbe9c7a1b0a9e
Kismat Ali <61203535+kismat5455@users.noreply.github.com>
2024-05-28 01:28:06 -0500

    feat: Add button to create new grid
    - Added a new button with id "newButton" inside a div with id "newGrid" to create a new grid.
    - When the button is clicked, it prompts the user to enter the size of the grid.
    - The grid container is cleared and a new grid is created based on the user's input size.

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
-//ca function that makes 16 *16 divs and appends them to the grid container
+//a function that makes 16 *16 divs and appends them to the grid container
+let count = 0;
+function createGrid(size = 16) {
+    while (size <= 0 || size > 100) {
+        alert('Please enter a number between 0 and 100');
+        size = Number(prompt('Enter the size of the grid'));
 
-function createGrid() {
+    }
     const container = document.querySelector('#grid-container');
-    for (let i = 0; i < 16; i++) {
-        for (let j = 0; j < 16; j++) {
+    const width = (805) / size
+    const height = (805) / size
+
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
             const div = document.createElement('div');
+            div.style.width = `${width}px`;
+            div.style.height = `${height}px`;
             container.appendChild(div);
+
         }
     }
 }
@@ -15,7 +26,22 @@ createGrid();
 const container = document.querySelector('#grid-container');
 container.addEventListener('mouseover', function (e) {
     if (e.target.id !== 'grid-container') {
+        count++;
         e.target.style.backgroundColor = 'black';
+        console.log(count);
     }
 
+});
+
+
+//ask a user to create new grid
+
+const button = document.querySelector('#newButton');
+
+button.addEventListener('click', function () {
+    let size = Number(prompt('Enter the size of the grid'));
+    const container = document.querySelector('#grid-container');
+    container.innerHTML = '';
+
+    createGrid(size);
 });
